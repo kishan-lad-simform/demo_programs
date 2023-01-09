1b89beb77360947753f39cf1b133f41ea78537bf
Kishan lad <kishan.l@simformsolution.com>
2023-01-09 16:18:26 +0530

    First change

---
+++ addition_ruby.rb
@@ -0,0 +1,4 @@
+number1=gets.chomp_to.i
+number2=gets.chomp_to.i
+number3=number1+number2
+p number3
---
+++ multiplication_ruby.rb
@@ -0,0 +1,6 @@
+puts "Enter number1 : "
+number1=gets.chomp_to.i
+puts "Enter number2 : "
+number2=gets.chomp_to.i
+number3=number1*number2
+puts "Multiplication is #{number3}"
